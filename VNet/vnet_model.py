1014000f943f046c6f62cc7a9c166de9144b71b9
julottevanderbeek <43807371+julottevanderbeek@users.noreply.github.com>
2024-10-09 12:20:30 +0200

    Update on VNet

--- VNet/vnet_model.py
+++ VNet/vnet_model.py
@@ -2,57 +2,74 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-
 class VNet(nn.Module):
-    def __init__(self, in_channels, out_channels):
+    def __init__(self, in_channels=1, num_classes=2):
         super(VNet, self).__init__()
-        self.enc1 = self.down_block(in_channels, 16)
-        self.enc2 = self.down_block(16, 32)
-        self.enc3 = self.down_block(32, 64)
-        self.enc4 = self.down_block(64, 128)
 
-        self.middle = self.double_conv(128, 256)
+        # Downsampling path
+        self.enc1 = self.encoder_block(in_channels, 16, use_pool=False)
+        self.enc2 = self.encoder_block(16, 32)
+        self.enc3 = self.encoder_block(32, 64)
+        self.enc4 = self.encoder_block(64, 128)
+        self.enc5 = self.encoder_block(128, 256)
 
-        self.dec4 = self.up_block(256, 128)
-        self.dec3 = self.up_block(128, 64)
-        self.dec2 = self.up_block(64, 32)
-        self.dec1 = self.up_block(32, 16)
+        # Upsampling path
+        self.dec4 = self.decoder_block(256, 128)
+        self.dec3 = self.decoder_block(128, 64)
+        self.dec2 = self.decoder_block(64, 32)
+        self.dec1 = self.decoder_block(32, 16)
 
-        self.final_conv = nn.Conv3d(16, out_channels, kernel_size=1)
+        # Final Convolution
+        self.final_conv = nn.Conv3d(16, num_classes, kernel_size=1)
 
-    def double_conv(self, in_channels, out_channels):
-        return nn.Sequential(
+        # Initialize weights
+        self.init_weights()
+
+    def encoder_block(self, in_channels, out_channels, use_pool=True):
+        layers = []
+        if use_pool:
+            layers.append(nn.MaxPool3d(2))
+        layers.extend([
             nn.Conv3d(in_channels, out_channels, kernel_size=3, padding=1),
-            nn.BatchNorm3d(out_channels),
             nn.ReLU(inplace=True),
             nn.Conv3d(out_channels, out_channels, kernel_size=3, padding=1),
-            nn.BatchNorm3d(out_channels),
+            nn.ReLU(inplace=True)
+        ])
+        return nn.Sequential(*layers)
+
+    def decoder_block(self, in_channels, out_channels):
+        return nn.Sequential(
+            nn.ConvTranspose3d(in_channels, out_channels, kernel_size=2, stride=2),
+            nn.Conv3d(out_channels, out_channels, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv3d(out_channels, out_channels, kernel_size=3, padding=1),
             nn.ReLU(inplace=True)
         )
 
-    def down_block(self, in_channels, out_channels):
-        return nn.Sequential(
-            nn.MaxPool3d(kernel_size=2),
-            self.double_conv(in_channels, out_channels)
-        )
-
-    def up_block(self, in_channels, out_channels):
-        return nn.Sequential(
-            nn.ConvTranspose3d(in_channels, out_channels, kernel_size=2, stride=2),
-            self.double_conv(out_channels, out_channels)
-        )
+    def init_weights(self):
+        for m in self.modules():
+            if isinstance(m, (nn.Conv3d, nn.ConvTranspose3d)):
+                nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='relu')
+                if m.bias is not None:
+                    nn.init.constant_(m.bias, 0)
+            elif isinstance(m, nn.BatchNorm3d):
+                nn.init.constant_(m.weight, 1)
+                nn.init.constant_(m.bias, 0)
 
     def forward(self, x):
-        enc1 = self.enc1(x)
-        enc2 = self.enc2(enc1)
-        enc3 = self.enc3(enc2)
-        enc4 = self.enc4(enc3)
+        # Encoder
+        enc1 = self.enc1(x)             # [B, 16, D, H, W]
+        enc2 = self.enc2(enc1)          # [B, 32, D/2, H/2, W/2]
+        enc3 = self.enc3(enc2)          # [B, 64, D/4, H/4, W/4]
+        enc4 = self.enc4(enc3)          # [B, 128, D/8, H/8, W/8]
+        enc5 = self.enc5(enc4)          # [B, 256, D/16, H/16, W/16]
 
-        middle = self.middle(enc4)
+        # Decoder with skip connections
+        dec4 = self.dec4(enc5) + enc4   # [B, 128, D/8, H/8, W/8]
+        dec3 = self.dec3(dec4) + enc3   # [B, 64, D/4, H/4, W/4]
+        dec2 = self.dec2(dec3) + enc2   # [B, 32, D/2, H/2, W/2]
+        dec1 = self.dec1(dec2) + enc1   # [B, 16, D, H, W]
 
-        dec4 = self.dec4(middle)
-        dec3 = self.dec3(dec4 + enc3)  # Skip connection
-        dec2 = self.dec2(dec3 + enc2)  # Skip connection
-        dec1 = self.dec1(dec2 + enc1)  # Skip connection
-
-        return self.final_conv(dec1)
+        # Final Convolution
+        out = self.final_conv(dec1)     # [B, num_classes, D, H, W]
+        return out
