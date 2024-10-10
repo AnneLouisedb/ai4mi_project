06738080d3649d64ad30517c8b15ed0acafc91f7
AnneLouisedb <annelouisedeboer@hotmail.com>
2024-10-10 23:29:33 +0200

    SUnet parts added

--- UNet/unet_parts.py
+++ UNet/unet_parts.py
@@ -75,3 +75,92 @@ class OutConv(nn.Module):
 
     def forward(self, x):
         return self.conv(x)
+
+####################
+# SUNET
+
+
+class Encoder_block(nn.Module):
+    """(convolution => ReLU =>[BN]) * 2"""
+
+    def __init__(self, in_channels, out_channels, mid_channels=None):
+        super().__init__()
+        if not mid_channels:
+            mid_channels = out_channels
+
+        self.first_conv = nn.Sequential(
+            nn.Conv2d(in_channels, mid_channels, kernel_size=3, padding=1, bias=False),
+            nn.ReLU(inplace=True),
+            nn.BatchNorm2d(mid_channels))
+        
+        self.second_conv = nn.Sequential(
+            nn.Conv2d(mid_channels, out_channels, kernel_size=3, padding=1, bias=False),
+            nn.ReLU(inplace=True),
+            nn.BatchNorm2d(out_channels))
+        
+        # Residual connection to match dimensions before concatenation
+        self.residual = nn.Conv2d(in_channels, mid_channels, kernel_size=1, padding=0)
+
+        self.maxpool =  nn.Sequential(
+            nn.MaxPool2d(2))
+
+    def forward(self, x):
+        
+        conv1_output = self.first_conv(x)
+        
+        conv2_output = self.second_conv(conv1_output)
+
+        residual_output = self.residual(conv1_output)
+
+        concat_output = torch.cat([conv2_output, residual_output], dim=1)
+        
+        # Max pooling
+        pooled_output = self.maxpool(concat_output)
+
+        return pooled_output
+    
+       
+class Decoder_block(nn.Module):
+
+    def __init__(self, in_channels, out_channels, bilinear=True):
+        super().__init__()
+
+        # if bilinear, use the normal convolutions to reduce the number of channels
+        if bilinear:
+            self.up = nn.Upsample(scale_factor=2, mode='bilinear', align_corners=True)
+
+            self.first_conv = nn.Sequential(
+                nn.Conv2d(in_channels, in_channels // 2, kernel_size=3, padding=1, bias=False),
+                nn.ReLU(inplace=True),
+                nn.BatchNorm2d(in_channels // 2))
+            
+            self.second_conv = nn.Sequential(
+                nn.Conv2d(in_channels // 2, out_channels, kernel_size=3, padding=1, bias=False),
+                nn.ReLU(inplace=True),
+                nn.BatchNorm2d(out_channels))
+
+            
+        # else:
+        #     self.up = nn.ConvTranspose2d(in_channels, in_channels // 2, kernel_size=2, stride=2)
+        #     self.conv = DoubleConv(in_channels, out_channels)
+
+    def forward(self, x1, x2):
+        x1 = self.up(x1)
+        # input is CHW
+        diffY = x2.size()[2] - x1.size()[2]
+        diffX = x2.size()[3] - x1.size()[3]
+
+        x1 = F.pad(x1, [diffX // 2, diffX - diffX // 2,
+                        diffY // 2, diffY - diffY // 2])
+        # if you have padding issues, see
+        # https://github.com/HaiyongJiang/U-Net-Pytorch-Unstructured-Buggy/commit/0e854509c2cea854e247a9c615f175f76fbb2e3a
+        # https://github.com/xiaopeng-liao/Pytorch-UNet/commit/8ebac70e633bac59fc22bb5195e513d5832fb3bd
+        x = torch.cat([x2, x1], dim=1)
+        
+        x = self.first_conv(x)
+        x = self.second_conv(x)
+        return x
+
+    
+
+
